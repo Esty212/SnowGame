4e2fb521c67370f3f7146bb698a5c54a7dec9264
Lilooshb <lilooshb@gmail.com>
2024-10-17 11:12:06 +0300

    Background Sound mute/unmute
    mute un mute background sound button is fixed

--- Assets/Scripts/GameMenu Scripts/SoundManager.cs
+++ Assets/Scripts/GameMenu Scripts/SoundManager.cs
@@ -11,10 +11,23 @@ public class SoundManager : MonoBehaviour
     private bool muted = false;
     void Start()
     {
-        
+        if (PlayerPrefs.HasKey("muted"))
+        {
+            PlayerPrefs.SetInt("muted", 0);
+            Load();
+        }
+
+        else
+        {
+            Load();
+        }
+
+        UpdateButtonIcon();
+        AudioListener.pause = muted;
     }
+
     public void OnButtonPress()
-    { 
+    {
         if (muted == false)
         {
             muted = true;
@@ -26,11 +39,32 @@ public class SoundManager : MonoBehaviour
             muted = false;
             AudioListener.pause = false;
         }
+        Save();
+        UpdateButtonIcon();
+
     }
 
-    private void Load()   // https://www.google.com/search?sca_esv=06cf028af5c7a812&cs=0&sxsrf=ADLYWIL5393qlp42YbkCvIA2kNRYeZwu3Q:1728922755505&q=Unity+mute+Audio+Listener&tbm=vid&source=lnms&fbs=AEQNm0BZDILf8XWKqSgqw26hAKzqx5LlKuFsoJhPXM59bIe4Uy-W2mQFSvoVhxpJ273EZTaWVlFYAfLGNRRPrpfngVtghSlSjle83RNTqxN3uZWh2PaEAwWOK5MH45mYvWZ5CqvRUZfHwJ1u9SfmiiXf2OrnLdmXxvVLeZG3ExyYTTHfxX053pVdd20SoW5wXD0lGY-DvjUs3aoERflpF9mDvB1tT-QNwg&sa=X&ved=2ahUKEwi8jb6Vo46JAxXaRvEDHS9tCTAQ0pQJegQIFRAB&biw=1163&bih=501&dpr=1.5#fpstate=ive&vld=cid:57040808,vid:AFcHsKd_aMo,st:0
-                        // min 3:07
+    private void UpdateButtonIcon()
     {
+        if (muted == false)
+        {
+            soundOnIcon.enabled = true;
+            soundOffIcon.enabled = false;
+        }
 
+        else
+        {
+            soundOnIcon.enabled = false;
+            soundOffIcon.enabled = true;
+        }
+    }
+    private void Load()
+    {
+        muted = PlayerPrefs.GetInt("muted") == 1;
+    }
+
+    private void Save()
+    {
+        PlayerPrefs.SetInt("muted", muted ? 1 : 0);
     }
 }
